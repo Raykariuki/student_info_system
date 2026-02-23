41bf87b9fa860f3c2fa92fc14b5c7474e787382b
Raymond Mbuthi <161636993+Raykariuki@users.noreply.github.com>
2026-02-23 23:45:44 +0300

    Delete Lists and Loops.py

--- Lists and Loops.py
+++
@@ -1,5 +0,0 @@
-students = ["Raymond", "Joseph", "Njanja", "Margret", "Natasha", 
-            "Upendo", "William", "Kenyatta", "Ryan"]
-
-for i in range(len(students)):
-    print(f"{i + 1}. {students[i].upper()}")
